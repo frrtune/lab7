b23c37344a9f38c18a811249f1777be01e2aa425
daria <bdrdria@yandex.ru>
2026-05-24 22:07:01 +0500

    feat: add get first, get last, get methods

--- lazy_sequence/lazy_sequence.hpp
+++ lazy_sequence/lazy_sequence.hpp
@@ -6,7 +6,7 @@
 #include "../generator/generator.hpp"
 #include "../optional/optional.hpp"
 
-template<template <typename> class Container = ArraySequence, typename T>
+template<typename T, template <typename> class Container = ArraySequence>
 class LazySequence {
     private:
         std::unique_ptr<Generator<T>> generator_;
@@ -15,15 +15,35 @@ class LazySequence {
         LazySequence() : generator_(nullptr) {}
         LazySequence (const T* items, size_t count) : generator_(nullptr) {
             for (size_t i = 0; i < count; i++) {
-                cache_.Append(items[i])
+                cache_.Append(items[i]);
             }
         } 
         LazySequence (Container<T>& sequence) : generator_(nullptr) {
             for (size_t i = 0; i < sequence.Getlength(); i++) {
-                cache_.Append(data.Get(i))
+                cache_.Append(sequence.Get(i));
             }
         }
-        LazySequence (const LazySequence<T>& other) : generator_(nullptr) {
+        LazySequence (const LazySequence<T>& other) : generator_(other.generator_), cache_(other.cache_) {};
+        /* реализовать генератор для функций
+        LazySequence (T(*function)(Sequence<T>*), Sequence<T> base)  {
 
         }
+        LazySequence (std::function<T(Sequence<T>&), Sequence<T>& base) {
+
+        }*/
+       T GetFirst() {
+            if (cache_.GetLength() == 0) throw EmptyBufferError("cache is empty");
+            return cache_.Get(0);
+       }
+       T GetLast() {
+            if (cache_.GetLength() == 0) throw EmptyBufferError("cache is empty");
+            return cache_.Get(cache_.GetLength() - 1);
+       }
+       T Get(size_t index) {
+            if (cache_.GetLength() == 0) throw EmptyBufferError("cache is empty");
+            if (index >= cache_.GetLength()) {
+                throw RangeError(index, cache_.GetLength());
+            } 
+            return cache_.Get(index);
+       }
 };
