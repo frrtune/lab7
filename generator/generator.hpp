fa2058a492cd816ca7ffb422d906957856c38d03
daria <bdrdria@yandex.ru>
2026-05-24 21:29:43 +0500

    feat: add constructors for lazy sequence & replaced unique pointers with shared in generator

--- generator/generator.hpp
+++ generator/generator.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "../lab5/sequence/sequence.hpp"
-//#include "../optional/optional.hpp"
+#include "../optional/optional.hpp"
 
 template <typename T>
 class Generator {
@@ -10,10 +10,10 @@ class Generator {
         virtual T GetNext() = 0;
         virtual bool HasNext() const = 0;
         virtual Optional<T> TryGetNext() = 0;
-        virtual std::unique_ptr<Generator<T>> Append(T& item) const = 0; 
-        virtual std::unique_ptr<Generator<T>> Append(Sequence<T>& items) const = 0; 
-        virtual std::unique_ptr<Generator<T>> Insert(T& item) const = 0; 
-        virtual std::unique_ptr<Generator<T>> Insert(Sequence<T>& items) const = 0; 
-        virtual std::unique_ptr<Generator<T>> Remove(T& item) const = 0; 
-        virtual std::unique_ptr<Generator<T>> Remove(Sequence<T>& items) const = 0; 
+        virtual std::shared_ptr<Generator<T>> Append(T& item) const = 0; 
+        virtual std::shared_ptr<Generator<T>> Append(Sequence<T>& items) const = 0; 
+        virtual std::shared_ptr<Generator<T>> Insert(T& item) const = 0; 
+        virtual std::shared_ptr<Generator<T>> Insert(Sequence<T>& items) const = 0; 
+        virtual std::shared_ptr<Generator<T>> Remove(T& item) const = 0; 
+        virtual std::shared_ptr<Generator<T>> Remove(Sequence<T>& items) const = 0; 
 };
--- lazy_sequence/lazy_sequence.hpp
+++ lazy_sequence/lazy_sequence.hpp
@@ -1,10 +1,29 @@
 #pragma once
 
 #include "../exceptions/exceptions.hpp"
-#include "../lab5/dynamic_array/dynamic_array.hpp"
+#include "../lab5/sequence/sequence.hpp"
+#include "../lab5/array_sequence/array_sequence.hpp"
+#include "../generator/generator.hpp"
+#include "../optional/optional.hpp"
 
-/*template<typename T>
+template<template <typename> class Container = ArraySequence, typename T>
 class LazySequence {
-        
-        
-}*/
+    private:
+        std::unique_ptr<Generator<T>> generator_;
+        Container<T> cache_;
+    public:
+        LazySequence() : generator_(nullptr) {}
+        LazySequence (const T* items, size_t count) : generator_(nullptr) {
+            for (size_t i = 0; i < count; i++) {
+                cache_.Append(items[i])
+            }
+        } 
+        LazySequence (Container<T>& sequence) : generator_(nullptr) {
+            for (size_t i = 0; i < sequence.Getlength(); i++) {
+                cache_.Append(data.Get(i))
+            }
+        }
+        LazySequence (const LazySequence<T>& other) : generator_(nullptr) {
+
+        }
+};
