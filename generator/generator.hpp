00032119feb19369c1a1028b5e919b85f716b789
daria <bdrdria@yandex.ru>
2026-05-25 04:07:54 +0500

    feat: add methods for ordinal generator

--- generator/generator.hpp
+++ generator/generator.hpp
@@ -10,10 +10,10 @@ class Generator {
         virtual T GetNext() = 0;
         virtual bool HasNext() const = 0;
         virtual Optional<T> TryGetNext() = 0;
-        virtual std::shared_ptr<Generator<T>> Append(T& item) const = 0; 
+        /*virtual std::shared_ptr<Generator<T>> Append(T& item) const = 0; 
         virtual std::shared_ptr<Generator<T>> Append(Sequence<T>& items) const = 0; 
         virtual std::shared_ptr<Generator<T>> Insert(T& item) const = 0; 
         virtual std::shared_ptr<Generator<T>> Insert(Sequence<T>& items) const = 0; 
         virtual std::shared_ptr<Generator<T>> Remove(T& item) const = 0; 
-        virtual std::shared_ptr<Generator<T>> Remove(Sequence<T>& items) const = 0; 
+        virtual std::shared_ptr<Generator<T>> Remove(Sequence<T>& items) const = 0;*/
 };
--- generator/ordinal_generator.hpp
+++ generator/ordinal_generator.hpp
@@ -7,6 +7,33 @@ class OrdinalGenerator : public Generator<Ordinal> {
     private:
         size_t mode_;
         size_t counter_;
+        static constexpr size_t LIMIT = 10;
     public:
         OrdinalGenerator() : mode_(0), counter_(0) {}
-}
+        Ordinal GetNext() override {
+            if (mode_ == 0) {
+                if (counter_ < LIMIT) {
+                    return Ordinal(counter_++);
+                }
+                mode_ = 1;
+                return Ordinal::omega();
+            }
+            if (mode_ == 1) {
+                mode_ = 2;
+                counter_ = 1;
+                return Ordinal::omega();
+            }
+            Ordinal result = Ordinal::omega() + counter_;
+            counter_++;
+            return result;
+        }
+        bool HasNext() const override {
+            return true;
+        }
+        Optional<Ordinal> TryGetNext() override {
+            if (!HasNext()) {
+                return Optional<Ordinal>();
+            }
+            return Optional<Ordinal>(GetNext());
+        }
+};
