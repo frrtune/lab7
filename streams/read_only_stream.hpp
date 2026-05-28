d0791ad107cd9554e8e15950c0da3245ee2b6ec3
daria <bdrdria@yandex.ru>
2026-05-29 02:37:50 +0500

    feat: add methods for read only stream

--- streams/read_only_stream.hpp
+++ streams/read_only_stream.hpp
@@ -2,6 +2,7 @@
 
 #include "../optional/optional.hpp"
 #include <fstream>
+#include <string>
 
 template<typename T>
 class ReadOnlyStream {
@@ -10,9 +11,27 @@ class ReadOnlyStream {
     public:
         ReadOnlyStream(std::string filename) {
             file_.open(filename);
-            if (!file_.is_open) throw StreamOpenError(filename);
+            if (!file_.is_open()) throw StreamOpenError(filename);
         }
         ~ReadOnlyStream() {
-            if (file_.is_open) file_.close();
+            if (file_.is_open()) file_.close();
+        }
+        bool is_end() {
+            return (file_.eof() || file_.fail());
+        }
+        Optional<T> try_read() {
+            if (is_end()) return Optional<T>();
+            return Optional<T>(read());
+        }
+        T read() {
+            T value;
+            file_ >> value;
+            if (file_.fail()) {
+                throw EndOfStreamError();
+            }
+            return value;
+        }
+        void close() {
+            file_.close();
         }
 };
