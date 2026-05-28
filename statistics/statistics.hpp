7dfade4a421f4f7eae80cab3729e41436e9663cc
daria <bdrdria@yandex.ru>
2026-05-29 03:27:00 +0500

    fix: replaced dynamic array with array sequence

--- statistics/statistics.hpp
+++ statistics/statistics.hpp
@@ -2,7 +2,7 @@
 
 #include "../streams/read_only_stream.hpp"
 #include "../optional/optional.hpp"
-#include "../lab5/dynamic_array/dynamic_array.hpp"
+#include "../lab5/array_sequence/array_sequence.hpp"
 
 template<typename T>
 class Statistics {
@@ -11,8 +11,8 @@ class Statistics {
         T sum_;
         T min_;
         T max_;
-        DynamicArray<T> values_;
-        size_t partition(DynamicArray<T>& arr, size_t low, size_t high) {
+        ArraySequence<T> values_;
+        size_t partition(ArraySequence<T>& arr, size_t low, size_t high) {
             T pivot = arr.Get(high);
             size_t i = low;
             for (size_t j = low; j < high; j++) {
@@ -28,7 +28,7 @@ class Statistics {
             arr.Set(high, temporary);
             return i;
         }
-        void quick_sort(DynamicArray<T>& arr, size_t low, size_t high) {
+        void quick_sort(ArraySequence<T>& arr, size_t low, size_t high) {
             if (low >= high) return;
             size_t i = partition(arr, low, high);
             if (i > low) quick_sort(arr, low, i - 1);
