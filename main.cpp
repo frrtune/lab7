066d999b3203f7c434c17b0796c1b32baf0c6ef6
daria <bdrdria@yandex.ru>
2026-06-02 04:17:46 +0500

    feat: add ordinal to string function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include "ui/ui.cpp"
+#include "lazy_sequence/lazy_sequence.hpp"
+#include "generator/ordinal_generator.hpp"
+#include "generator/insert_generator.hpp"
+#include "generator/concat_generator.hpp"
+#include "generator/natural_numbers_generator.hpp"
+#include <iostream>
+#include <memory>
 
-int main() {
-    open_ui();
-    return 0;
+std::string ordinal_to_string(const Ordinal& o) {
+    if (o.IsZero()) return "0";
+    if (o.IsFinite()) return std::to_string(o.GetValue());
+    if (o.IsOmega()) return "w";
+    if (o.IsOmegaPlusN()) return "w+" + std::to_string(o.GetOmegaPlusN());
+    return "";
 }
