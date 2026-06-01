e17b740984a6b33a346a92fa545f9bea10c15fcb
daria <bdrdria@yandex.ru>
2026-06-01 17:02:50 +0500

    feat: add get next, has next and try get next methods

--- generator/insert_generator.hpp
+++ generator/insert_generator.hpp
@@ -12,4 +12,23 @@ class InsertGenerator : public Generator<T> {
     public:
         InsertGenerator(std::shared_ptr<Generator<T>> first, std::shared_ptr<Generator<T>> second, size_t insert_index) :
         first_(first), second_(second), insert_index_(insert_index), current_index_(0) {}
+        T GetNext() override {
+            if (current_index_ < insert_index_) {
+                current_index_++;
+                return first_->GetNext();
+            }
+            return second_->GetNext();
+        }
+        bool HasNext() const override {
+            if (current_index_ < insert_index_) {
+                return first_->HasNext();
+            }
+            return second_->HasNext();
+        }
+        Optional<T> TryGetNext() override {
+            if (!HasNext()) {
+                return Optional<T>();
+            }
+            return Optional<T>(GetNext());
+        }
 };
