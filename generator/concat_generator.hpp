f8b042350d099e410fa888769dc89250f187053e
daria <bdrdria@yandex.ru>
2026-06-02 01:45:12 +0500

    feat: add next methods and fixed constructor name after copying it from insert_generator.hpp

--- generator/concat_generator.hpp
+++ generator/concat_generator.hpp
@@ -9,6 +9,25 @@ class ConcatGenerator : public Generator<T> {
         std::shared_ptr<Generator<T>> second_;
         bool first_done_; 
     public:
-        InsertGenerator(std::shared_ptr<Generator<T>> first, std::shared_ptr<Generator<T>> second) :
+        ConcatGenerator(std::shared_ptr<Generator<T>> first, std::shared_ptr<Generator<T>> second) :
         first_(first), second_(second), first_done_(false) {}
+         T GetNext() override {
+            if (!first_done_ && first_->HasNext()) {
+                return first_->GetNext();
+            }
+            first_done_ = true;
+            return second_->GetNext();
+        }
+        bool HasNext() const override {
+            if (!first_done_ && first_->HasNext()) {
+                return true;
+            }
+            return second_->HasNext();
+        }
+        Optional<T> TryGetNext() override {
+            if (!HasNext()) {
+                return Optional<T>();
+            }
+            return Optional<T>(GetNext());
+        }
 };
