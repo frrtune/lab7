21cce6ff9cadda76ef6eab60ab78d38c1692813b
daria <bdrdria@yandex.ru>
2026-06-02 02:22:24 +0500

    feat: add next methods and removed template

--- generator/natural_numbers_generator.hpp
+++ generator/natural_numbers_generator.hpp
@@ -2,11 +2,23 @@
 
 #include "generator.hpp"
 
-template<typename T>
-class NaturalNumbersGenerator : public Generator<T> {
+class NaturalNumbersGenerator : public Generator<long long> {
     private:
         long long current_;
         long long limit_;
     public:
         NaturalNumbersGenerator(long long limit) : current_(0), limit_(limit) {}
+        long long GetNext() override {
+            if (current_ >= limit_) throw InvalidArgumentError("limit has been reached");
+            return current_++;
+        }
+        bool HasNext() const override {
+            return current_ < limit_;
+        }
+        Optional<long long> TryGetNext() override {
+            if (!HasNext()) {
+                return Optional<long long>();
+            }
+            return Optional<long long>(GetNext());
+        }
 };
