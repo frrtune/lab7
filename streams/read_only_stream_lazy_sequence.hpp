026a55a9a0962630b8fd2f42fc5e9e21ea0bc6df
daria <bdrdria@yandex.ru>
2026-06-03 02:09:42 +0500

    feat: add constructor & is_end method

--- streams/read_only_stream_lazy_sequence.hpp
+++ streams/read_only_stream_lazy_sequence.hpp
@@ -1,14 +1,21 @@
 #pragma once
 
+#include "../lazy_sequence/lazy_sequence.hpp"
 #include "../streams/read_only_stream.hpp"
 
 template<typename T>
 class ReadOnlyStreamLazySequence : public ReadOnlyStream<T> {
     private:
         LazySequence<T>* sequence_;
+        size_t current_;
     public:
-        ~ReadOnlyStreamLazySequence() = default;
-        bool is_end() override {}
+        ReadOnlyStreamLazySequence(LazySequence<T>* sequence) : sequence_(sequence), current_(0) {}
+        bool is_end() override {
+            try {
+                sequence_->Get(current_);
+                return false;
+            } catch (const RangeError&) return true;
+        }
         Optional<T> try_read() override {};
         T read() override {};
         void close() override {};
