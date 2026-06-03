831748b2f90d242b60f0ce7aa4d61556fa70a2b1
daria <bdrdria@yandex.ru>
2026-06-03 05:44:07 +0500

    feat: add next methods

--- generator/insert_at_generator.hpp
+++ generator/insert_at_generator.hpp
@@ -7,5 +7,33 @@ class InsertAtGenerator : public Generator<T> {
     private:
         std::shared_ptr<Generator<T>> generator_;
         T item_;
-        bool done_; 
+        bool done_;
+        size_t index_;
+        size_t current_;
+    public:
+        InsertAtGenerator(std::shared_ptr<Generator<T>> generator, T item, size_t index) : 
+        generator_(generator), item_(item), done_(false), index_(index), current_(0) {}
+        T GetNext() override {
+            if (current_ < index_ && generator_->HasNext()) {
+                current_++;
+                return generator_->GetNext();
+            }
+            if (!done_) {
+                done_ = true;
+                return item_;
+            }
+            if (generator_->HasNext()) return generator_->GetNext();
+            throw EmptyBufferError();
+        }
+        bool HasNext() const override {
+            if (current_ < index_ && generator_->HasNext()) return true;
+            if (!done_) return true;
+            return generator_->HasNext();
+        }
+        Optional<T> TryGetNext() override {
+            if (!HasNext()) {
+                return Optional<T>();
+            }
+            return Optional<T>(GetNext());
+        }
 };
