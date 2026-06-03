0fa198a6657a3379bf2f20e2ed8b26dc2e9d5ce9
daria <bdrdria@yandex.ru>
2026-06-03 05:12:36 +0500

    feat: add next methods

--- generator/append_generator.hpp
+++ generator/append_generator.hpp
@@ -9,5 +9,25 @@ class AppendGenerator : public Generator<T> {
         T item_;
         bool done_; 
     public:
-        AppendGenerator(std::shared_ptr<Generator<T>> generator, I item) : generator_(generator), done_(false) {}
+        AppendGenerator(std::shared_ptr<Generator<T>> generator, T item) : 
+        generator_(generator), item_(item), done_(false) {}
+        T GetNext() override {
+            if (generator_->HasNext()) {
+                return generator_->GetNext();
+            }
+            if (!done_) {
+                done_ = true;
+                return item;
+            }
+            throw EmptyBufferError();
+        }
+        bool HasNext() const override {
+            return !done_;
+        }
+        Optional<T> TryGetNext() override {
+            if (!HasNext()) {
+                return Optional<T>();
+            }
+            return Optional<T>(GetNext());
+        }
 };
