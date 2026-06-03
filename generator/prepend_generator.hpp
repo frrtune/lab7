32d18370b3db23fd4e962d7811bd48abd42be226
daria <bdrdria@yandex.ru>
2026-06-03 05:23:25 +0500

    feat: add methods

--- generator/prepend_generator.hpp
+++ generator/prepend_generator.hpp
@@ -7,4 +7,27 @@ class PrependGenerator : public Generator<T> {
     private:
         std::shared_ptr<Generator<T>> generator_;
         T item_;
+        bool done_;
+    public:
+        PrependGenerator(std::shared_ptr<Generator<T>> generator, T item) : 
+        generator_(generator), item_(item), done_(false) {}
+        T GetNext() override {
+            if (!done_) {
+                done_ = true;
+                return item;
+            }
+            if (generator_->HasNext()) {
+                return generator_->GetNext();
+            }
+            throw EmptyBufferError();
+        }
+        bool HasNext() const override {
+            return !done_ || generator_->HasNext();
+        }
+        Optional<T> TryGetNext() override {
+            if (!HasNext()) {
+                return Optional<T>();
+            }
+            return Optional<T>(GetNext());
+        }
 };
